16745d9c52daa471911316d43f3c143c8189cf56
Alexander Zent <azent@gmx.de>
2018-06-05 14:00:51 +0200

    initial checkin - Bar.java

--- src/main/java/de/zent/giteval/App.java
+++ src/main/java/de/zent/giteval/App.java
@@ -2,12 +2,15 @@ package de.zent.giteval;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
+
+    public String echo(String name){
+        return "Hello " + name + "!";
+    }
+
 }
---
+++ src/main/java/de/zent/giteval/Util.java
@@ -0,0 +1,9 @@
+package de.zent.giteval;
+
+public class Util {
+
+    public String echo(String name){
+        return "Hello " + name + "!";
+    }
+
+}
